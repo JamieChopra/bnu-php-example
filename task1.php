b66db22a1e88deea6f5e2eb43b935b7353cb7fbc
JamieChopra <JamieChopra@users.noreply.github.com>
2022-05-03 10:56:48 +0100

    Update task1.php

--- task1.php
+++ task1.php
@@ -9,26 +9,34 @@
       $sql = "INSERT INTO student (studentid, password, dob, firstname, lastname, house, town, county, country, postcode)
       VALUES('10895872', 'E8>MNgQvhtte#;,Z', '1959-08-12', 'Travon', 'Walker', '11 Park Road', 'Jacksonville', 'Greater Manchester', 'England', 'M1 2BR')";
       $result = mysqli_query($conn,$sql);
-      
+   }
+   
+   if (isset($_SESSION['id'])) {
       //Data 2
       $sql = "INSERT INTO student (studentid, password, dob, firstname, lastname, house, town, county, country, postcode)
       VALUES('04206948', '4V+d;tDh2hzcSj', '2000-09-11', 'Aidan', 'Hutchinson', '21 Hook Street', 'Plymouth', 'Essex', 'England', CM3 8BR')";
       $result = mysqli_query($conn,$sql);
+   }
 
+   if (isset($_SESSION['id'])) {
       //Data 3
       $sql = "INSERT INTO student (studentid, password, dob, firstname, lastname, house, town, county, country, postcode)
       VALUES('60451902', 'cVnq:w>d>.w3B:>@', '2001-06-20', 'Derek', 'Stingley Jr.', '35 Butcher Road', 'Baton Rouge', 'Louisiana', 'United States', LS5 WIN')";
       $result = mysqli_query($conn,$sql);
+   }
 
+   if (isset($_SESSION['id'])) {
       //Data 4
       $sql = "INSERT INTO student (studentid, password, dob, firstname, lastname, house, town, county, country, postcode)
       VALUES('02625680', 'Hh/)hE=HNy`#3mR', '2000-09-31', 'Ahmad', 'Gardner', '14 Power Road', 'Detroit', 'Michigan', 'United States', MI5 1NA')";
       $result = mysqli_query($conn,$sql);
+   }
 
+   if (isset($_SESSION['id'])) {
       //Data 5
       $sql = "INSERT INTO student (studentid, password, dob, firstname, lastname, house, town, county, country, postcode)
       VALUES('67904608', 'F3~Z4F})yy[n]#u+', '2000-12-15', 'Kayvon', 'Thibodeaux', '99 GOAT Street', 'Los Angeles', 'California', 'United States', LA1 5XB')";
       $result = mysqli_query($conn,$sql);
-
-    }
+   }
+    
 ?>
