52e23f88a145566a1c6e84d8904fb1ce20700a8d
JamieChopra <JamieChopra@users.noreply.github.com>
2022-05-03 13:45:15 +0100

    Task 2

--- students.php
+++ students.php
@@ -12,18 +12,27 @@
       echo template("templates/partials/nav.php");
 
       // Build SQL statment that selects a student's modules
-      $sql = "select * from studentmodules sm, module m where m.modulecode = sm.modulecode and sm.studentid = '" . $_SESSION['id'] ."';";
+      $sql = "select * from student";
 
       $result = mysqli_query($conn,$sql);
 
       // prepare page content
       $data['content'] .= "<table border='1'>";
-      $data['content'] .= "<tr><th colspan='5' align='center'>Modules</th></tr>";
-      $data['content'] .= "<tr><th>Code</th><th>Type</th><th>Level</th></tr>";
+      $data['content'] .= "<tr><th colspan='50' align='center'>Students</th></tr>";
+      $data['content'] .= "<tr><th>Student ID</th><th>Password</th><th>Date of Birth</th><th>First Name</th><th>Last Name</th>
+      <th>House</th><th>Town</th><th>County</th><th>Country</th><th>Postcode</th></tr>";
       // Display the modules within the html table
       while($row = mysqli_fetch_array($result)) {
-         $data['content'] .= "<tr><td> $row[modulecode] </td><td> $row[name] </td>";
-         $data['content'] .= "<td> $row[level] </td></tr>";
+         $data['content'] .= "<tr><td> $row[studentid] </td>";
+         $data['content'] .= "<td> $row[password] </td>";
+         $data['content'] .= "<td> $row[dob] </td>";
+         $data['content'] .= "<td> $row[firstname] </td>";
+         $data['content'] .= "<td> $row[lastname] </td>";
+         $data['content'] .= "<td> $row[house] </td>";
+         $data['content'] .= "<td> $row[town] </td>";
+         $data['content'] .= "<td> $row[county] </td>";
+         $data['content'] .= "<td> $row[country] </td>";
+         $data['content'] .= "<td> $row[postcode] </td></tr>";
       }
       $data['content'] .= "</table>";
 
